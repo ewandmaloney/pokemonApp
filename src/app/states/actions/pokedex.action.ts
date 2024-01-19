7167ffc5eced76d76ff773a0604235c29fe1d7df
Maloney <emaloney@Almato.com>
2024-01-19 11:31:39 +0100

    Changes made

--- src/app/states/actions/pokedex.action.ts
+++ src/app/states/actions/pokedex.action.ts
@@ -1,8 +1,9 @@
 import { createAction, props } from "@ngrx/store";
+import { PokemonState } from "../app.state";
 
 export const loadPokedex = createAction('[Pokedex] Load Pokedex');
 export const setPokedex = createAction('[Pokedex] Set Pokedex',
-    props<{ pokemons: any[] }>()
+    props<{ pokemons: PokemonState[] }>()
 );
 // export const addPokemon = createAction('[Pokedex] Add Pokemon',
 //     props<{ pokemon: any }>()
